6dc293b35de92076acfc6f303ae638afa8398ad5
mrimac-1985 <maximorimacayala@gmail.com>
2020-11-17 14:57:58 -0600

    Se ordenó las tablas

--- 01_Tablas/01_Tablas.sql
+++ 01_Tablas/01_Tablas.sql
@@ -1,4 +1,342 @@
----tablas comentario
+-- public.auditoria_procedimiento definition
+
+-- Drop table
+
+-- DROP TABLE public.auditoria_procedimiento;
+
+CREATE TABLE public.auditoria_procedimiento (
+	nidaudproced serial NOT NULL,
+	spalabraclave varchar(100) NOT NULL,
+	sdata varchar(500) NOT NULL,
+	dfechareg timestamp NOT NULL,	
+	CONSTRAINT pk_auditoria_procedimiento PRIMARY KEY (nidaudproced)
+);
+
+
+-- public.info_sistema definition
+
+-- Drop table
+
+-- DROP TABLE public.info_sistema;
+
+CREATE TABLE public.info_sistema (
+	nidinfosistema serial NOT NULL,
+	slogo bytea NULL,
+	snombresistema varchar(100) NOT NULL,
+	sversion varchar(50) NOT NULL,
+	bactivo bool NOT NULL,
+	dfechareg timestamp NOT NULL,
+	CONSTRAINT pk_info_sistema  PRIMARY KEY (nidinfosistema)
+);
+
+
+-- public.modulo definition
+
+-- Drop table
+
+-- DROP TABLE public.modulo;
+
+CREATE TABLE public.modulo (
+	nidmodulo serial NOT NULL,	
+	snombremodulo varchar(100) NOT NULL,
+	ssiglas varchar(10) NOT NULL,
+	sobservacion varchar(500) NOT NULL,		
+	bactivo bool NOT NULL,
+	dfechareg timestamp NOT NULL,
+	nidsesion int4 NULL,
+	CONSTRAINT pk_modulo PRIMARY KEY (nidmodulo)
+);
+
+
+-- public.operador definition
+
+-- Drop table
+
+-- DROP TABLE public.operador;
+
+CREATE TABLE public.operador (
+	nidoperador serial NOT NULL,
+	snumdocu varchar(15) NOT NULL,
+	snombre varchar(80) NOT NULL,
+	sapepaterno varchar(80) NOT NULL,	
+	sapematerno varchar(80) NOT NULL,
+	sgenero varchar(1) NOT NULL,
+	dfechanac date NOT NULL,
+	sobservacion varchar(500) NULL,
+	bactivo bool NOT NULL,
+	dfechareg timestamp NOT NULL,
+	nidsesion int4 NULL,
+	CONSTRAINT pk_operador  PRIMARY KEY (nidoperador)
+);
+
+
+-- public.parametro definition
+
+-- Drop table
+
+-- DROP TABLE public.parametro;
+
+CREATE TABLE public.parametro (
+	nidparametro serial NOT NULL,
+	bactivo bool NOT NULL,
+	dfechareg timestamp NOT NULL,
+	sclave varchar(50) NOT NULL,
+	sdescripcion varchar(200) NOT NULL,
+	svalor varchar(100) NOT NULL,
+	CONSTRAINT pk_parametro PRIMARY KEY (nidparametro)
+);
+
+
+-- public.perfil definition
+
+-- Drop table
+
+-- DROP TABLE public.perfil;
+
+CREATE TABLE public.perfil (
+	nidperfil serial NOT NULL,	
+	snombreperfil varchar(80) NOT NULL,
+	nsesionesporusuario int4 NOT NULL,
+	ntiempoconexionminuto int4 NOT NULL,
+	ntiempovidapasworddia int4 NOT NULL,	
+	bactivo bool NOT NULL,
+	dfechareg timestamp NOT NULL,
+	nidsesion int4 NULL,
+	CONSTRAINT pk_perfil PRIMARY KEY (nidperfil)
+);
+
+
+-- public.rol definition
+
+-- Drop table
+
+-- DROP TABLE public.rol;
+
+CREATE TABLE public.rol (
+	nidrol serial NOT NULL,
+	snombrerol varchar(100) NOT NULL,	
+	siglas varchar(20) NOT NULL,	
+	sobservacion varchar(500) NOT NULL,
+	bactivo bool NOT NULL,
+	dfechareg timestamp NOT NULL,
+	nidsesion int4 NULL,
+	CONSTRAINT pk_rol PRIMARY KEY (nidrol)
+);
+
+
+-- public.sede definition
+
+-- Drop table
+
+-- DROP TABLE public.sede;
+
+CREATE TABLE public.sede (
+	nidsede serial NOT NULL,	
+	snombre varchar(100) NOT NULL,
+	sdireccion varchar(150) NOT NULL,	
+	subigeo varchar(50) NOT NULL,
+	bactivo bool NOT NULL,
+	dfechareg timestamp NOT NULL,
+	nidsesion int4 NULL,
+	CONSTRAINT pk_sede PRIMARY KEY (nidsede)
+);
+
+
+
+
+-- public.area definition
+
+-- Drop table
+
+-- DROP TABLE public.area;
+
+CREATE TABLE public.area (
+	nidarea serial NOT NULL,
+	snombre varchar(100) NOT NULL,	
+	sobservacion varchar(500) NULL,
+	nidsede int4 NOT NULL,
+	bactivo bool NOT NULL,
+	dfechareg timestamp NOT NULL,
+	nidsesion int4 NULL,
+	CONSTRAINT pk_area PRIMARY KEY (nidarea),
+	CONSTRAINT fk_area_nidsede_sede_nidsede FOREIGN KEY (nidsede) REFERENCES sede(nidsede)
+);
+
+
+-- public.menu definition
+
+-- Drop table
+
+-- DROP TABLE public.menu;
+
+CREATE TABLE public.menu (
+	nidmenu serial NOT NULL,	
+	norden int4 NOT NULL,
+	snombremenu varchar(100) NOT NULL,	
+	siconomenu varchar(50) NOT NULL,	
+	surl varchar(50) NOT NULL,
+	nidmodulo int4 NOT NULL,
+	bactivo bool NULL,
+	dfechareg timestamp NOT NULL,
+	nidsesion int4 NULL,
+	CONSTRAINT pk_menu PRIMARY KEY (nidmenu),
+	CONSTRAINT fk_menu_nidmodulo_modulo_nidmodulo FOREIGN KEY (nidmodulo) REFERENCES modulo(nidmodulo)
+);
+
+
+-- public.menu_rol definition
+
+-- Drop table
+
+-- DROP TABLE public.menu_rol;
+
+CREATE TABLE public.menu_rol (	
+	nidmenu int4 NOT NULL,
+	nidrol int4 NOT NULL,	
+	bactivo bool NOT NULL,
+	dfechareg timestamp NOT NULL,
+	CONSTRAINT pk_menu_rol PRIMARY KEY (nidmenu, nidrol),
+	CONSTRAINT fkh92d5xc36jg9ks78efnwuml3e FOREIGN KEY (nidmenu) REFERENCES menu(nidmenu),
+	CONSTRAINT fki4bpv0jms2qlc7yq69u7eqgr8 FOREIGN KEY (nidrol) REFERENCES rol(nidrol)
+);
+
+
+-- public.submenu definition
+
+-- Drop table
+
+-- DROP TABLE public.submenu;
+
+CREATE TABLE public.submenu (
+	nidsubmenu serial NOT NULL,
+	norden int4 NOT NULL,
+	snombremenu varchar(100) NOT NULL,	
+	siconomenu varchar(50) NOT NULL,	
+	surl varchar(50) NOT NULL,
+	nidmenu int4 NULL,
+	CONSTRAINT pk_submenu PRIMARY KEY (nidsubmenu),
+	CONSTRAINT fk_menu_nidmenu_submenu_nidmenu FOREIGN KEY (nidmenu) REFERENCES menu(nidmenu)
+);
+
+
+-- public.usuario definition
+
+-- Drop table
+
+-- DROP TABLE public.usuario;
+
+CREATE TABLE public.usuario (
+	nidusuario int4 NOT NULL,	
+	slogin varchar(50) NOT NULL,
+	spassword varchar(100) NOT NULL,	
+	bactivo bool NOT NULL,
+	dfechareg timestamp NOT NULL,
+	nidsesion int4 NULL,
+	CONSTRAINT pk_usuario PRIMARY KEY (nidusuario),
+	CONSTRAINT pk_usuario_slogin UNIQUE (slogin),
+	CONSTRAINT fk_usuario_nidusuario_operador_nidoperador FOREIGN KEY (nidusuario) REFERENCES operador(nidoperador)
+);
+
+
+-- public.usuario_area definition
+
+-- Drop table
+
+-- DROP TABLE public.usuario_area;
+
+CREATE TABLE public.usuario_area (
+	nidusuarioarea serial NOT NULL,
+	nidarea int4 NOT NULL,	
+	sobservacion varchar(500) NOT NULL,
+	nidusuario int4 NOT NULL,
+	bactivo bool NOT NULL,
+	dfechareg timestamp NOT NULL,
+	nidsesion int4 NULL,
+	CONSTRAINT pk_usuario_area PRIMARY KEY (nidusuarioarea),
+	CONSTRAINT fk_usuarioarea_nidarea_area_nidarea FOREIGN KEY (nidarea) REFERENCES area(nidarea),
+	CONSTRAINT fk_usuarioarea_nidusuario_usuario_nidusuario FOREIGN KEY (nidusuario) REFERENCES usuario(nidusuario)
+);
+
+
+-- public.usuario_biometria definition
+
+-- Drop table
+
+-- DROP TABLE public.usuario_biometria;
+
+CREATE TABLE public.usuario_biometria (
+	nidusuariobiometria serial NOT NULL,	
+	nidusuario int4 NOT NULL,
+	stipoimagen varchar(25) NOT NULL,
+	ximagen bytea NOT NULL,	
+	bactivo bool NOT NULL,
+	dfechareg timestamp NOT NULL,
+	nidsesion int4 NULL,
+	CONSTRAINT pk_usuario_biometria PRIMARY KEY (nidusuariobiometria),
+	CONSTRAINT fk_usuariobiometria_nidusuario_usuario_nidusuario FOREIGN KEY (nidusuario) REFERENCES usuario(nidusuario)
+);
+
+
+-- public.usuario_perfil definition
+
+-- Drop table
+
+-- DROP TABLE public.usuario_perfil;
+
+CREATE TABLE public.usuario_perfil (
+	nidusuarioperfil serial NOT NULL,	
+	nidusuario int4 NOT NULL,
+	nidperfil int4 NOT NULL,
+	sobservacion varchar(500) NULL,	
+	bactivo bool NOT NULL,
+	dfechareg timestamp NOT NULL,
+	nidsesion int4 NULL,
+	CONSTRAINT pk_usuario_perfil PRIMARY KEY (nidusuarioperfil),
+	CONSTRAINT fk_usuarioperfil_nidperfil_perfil_nidperfil FOREIGN KEY (nidperfil) REFERENCES perfil(nidperfil),
+	CONSTRAINT fk_usuarioperfil_nidusuario_usuario_nidusuario FOREIGN KEY (nidusuario) REFERENCES usuario(nidusuario)
+);
+
+
+-- public.usuario_rol definition
+
+-- Drop table
+
+-- DROP TABLE public.usuario_rol;
+
+CREATE TABLE public.usuario_rol (
+	nidusuariorol serial NOT NULL,
+	nidusuario int4 NOT NULL,
+	nidrol int4 NOT NULL,
+	sobservacion varchar(500) NOT NULL,	
+	bactivo bool NOT NULL,
+	dfechareg timestamp NOT NULL,
+	nidsesion int4 NULL,
+	CONSTRAINT pk_usuario_rol PRIMARY KEY (nidusuariorol),
+	CONSTRAINT fk_usuariorol_nidrol_rol_nidrol FOREIGN KEY (nidrol) REFERENCES rol(nidrol),
+	CONSTRAINT fk_usuariorol_nidusuario_usuario_nidusuario FOREIGN KEY (nidusuario) REFERENCES usuario(nidusuario)
+);
+
+
+-- public.sesion definition
+
+-- Drop table
+
+-- DROP TABLE public.sesion;
+
+CREATE TABLE public.sesion (
+	nidsesion serial NOT NULL,
+	bactivo bool NOT NULL,
+	dfechafin date NULL,
+	dfechainicio date NOT NULL,
+	dfechareg timestamp NOT NULL,
+	ssistemaversion varchar(50) NOT NULL,
+	nidusuario int4 NOT NULL,
+	CONSTRAINT pk_sesion PRIMARY KEY (nidsesion),
+	CONSTRAINT fk_sesion_nidusuario_usuario_nidusuario FOREIGN KEY (nidusuario) REFERENCES usuario(nidusuario)
+);
+
+
+/*tablas para el tocken*/
 
 create table oauth_access_token (
   token_id VARCHAR(256),
--- 04_Funciones/04_Funciones.sql
+++ 04_Funciones/04_Funciones.sql
@@ -108,3 +108,279 @@ LANGUAGE plpgsql;
 
 
 /*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
+
+
+
+
+CREATE OR REPLACE FUNCTION public.fn_eliminar_perfil(p_idperfil integer)
+ RETURNS character varying
+ LANGUAGE plpgsql
+AS $function$
+DECLARE 
+	v_respuesta varchar;
+	v_perfil perfil%rowtype;
+BEGIN
+	BEGIN
+		SELECT * FROM perfil
+		INTO v_perfil
+		WHERE nidperfil = p_idperfil;
+	
+		IF NOT FOUND THEN
+			v_respuesta = 'No se encontro id perfil en el registro';
+		ELSE
+			UPDATE 
+				public.perfil
+			SET
+				bactivo = FALSE
+			WHERE 
+				nidperfil = p_idperfil;			
+			v_respuesta	:= 'Se eliminó perfil correctamente';
+		END IF;
+	EXCEPTION 
+		 WHEN NO_DATA_FOUND THEN 
+		 v_respuesta := 'No se encontró registro en la funcion fn_eliminar_perfil --'||SUBSTR(SQLERRM,1,200);
+		 WHEN OTHERS THEN 
+		 v_respuesta := 'Error en fn_eliminar_perfil --'||SUBSTR(SQLERRM,1,200);
+	END;
+	RETURN v_respuesta;
+END
+$function$
+;
+
+
+
+
+
+/*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
+
+
+
+CREATE OR REPLACE FUNCTION public.fn_sist_actualiza_usuario(p_nidusuario integer, p_slogin character varying, p_spassword character varying, p_dfechanac date, p_sapematerno character varying, p_sapepaterno character varying, p_sgenero character varying, p_snombre character varying, p_snumdocu character varying, p_sobservacion character varying, p_nidarea integer, p_nidperfil integer, p_nidrol integer)
+ RETURNS character varying
+ LANGUAGE plpgsql
+AS $function$
+DECLARE 
+	v_respuesta varchar;
+	
+BEGIN
+
+	BEGIN
+		/*ACTUALIZA USUARIO*/
+		UPDATE 
+		  public.usuario 
+		SET 
+		  slogin = p_slogin,
+		  spassword = p_spassword
+		WHERE 
+		  nidusuario = p_nidusuario;
+		  
+		/*ACTUALIZA OPERADOR*/
+		UPDATE 
+		  public.operador 
+		SET 
+		  dfechanac = p_dfechanac,
+		  sapematerno = p_sapematerno,
+		  sapepaterno = p_sapepaterno,
+		  sgenero = p_sgenero,
+		  snombre = p_snombre,
+		  snumdocu = p_snumdocu,
+		  sobservacion = p_sobservacion
+		WHERE 
+		  nidoperador = p_nidusuario;
+
+
+		/*ACTUALIZA PERFIL*/
+		UPDATE 
+		  public.usuario_perfil 
+		SET 
+		  nidperfil = p_nidperfil
+		WHERE 
+		 nidusuario = p_nidusuario;
+
+
+		/*ACTUALIZA AREA*/
+		UPDATE 
+		  public.usuario_area 
+		SET 
+		  nidarea = p_nidarea
+		WHERE 
+			nidusuario = p_nidusuario;	
+
+		/*ACTUALIZA ROL*/            
+         UPDATE 
+          public.usuario_rol 
+        SET 
+          nidrol = p_nidrol
+        WHERE 
+          nidusuario = p_nidusuario;
+            
+            
+        /*RESPUESTA SQL*/    	  		  
+		v_respuesta	:= 'Se realizó la actualización de usuario';
+
+	EXCEPTION 
+		 WHEN OTHERS THEN 
+		 v_respuesta := 'Error en fn_sist_actualiza_usuario --'||SUBSTR(SQLERRM,1,200);
+    END;
+    
+	RETURN v_respuesta;
+END		 
+$function$
+;
+
+
+
+/*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
+
+
+CREATE OR REPLACE FUNCTION public.fn_sist_consulta_area_usuario(integer)
+ RETURNS integer
+ LANGUAGE plpgsql
+AS $function$
+DECLARE 
+    v_id_area INTEGER;
+BEGIN
+
+	/*CONSULA AREA*/
+    SELECT 
+      public.usuario_area.nidarea INTO v_id_area
+    FROM
+      public.usuario
+      INNER JOIN public.usuario_area ON (public.usuario.nidusuario = public.usuario_area.nidusuario)
+      WHERE 
+      	public.usuario.nidusuario = $1 AND public.usuario_area.bactivo= TRUE;
+    
+    RETURN v_id_area;
+END
+$function$
+;
+
+
+
+
+/*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
+
+
+CREATE OR REPLACE FUNCTION public.fn_sist_consulta_perfil_usuario(integer)
+ RETURNS SETOF usuario_perfil
+ LANGUAGE plpgsql
+AS $function$
+DECLARE 
+	t_usuario_area usuario_area;
+    
+BEGIN
+
+	/*CONSULA PERFIL*/       
+    FOR t_usuario_area IN
+        SELECT 
+          usuario_perfil.nidusuarioperfil,
+          usuario_perfil.bactivo,
+          usuario_perfil.dfechareg,
+          usuario_perfil.nidsesion,
+          usuario_perfil.sobservacion,
+          usuario_perfil.nidperfil,
+          usuario_perfil.nidusuario
+        FROM 
+          public.usuario_perfil
+        INNER JOIN public.usuario ON (public.usuario_perfil.nidusuario = public.usuario.nidusuario)
+        WHERE public.usuario.nidusuario = $1 AND public.usuario_perfil.bactivo= TRUE
+        LOOP
+    	RETURN NEXT t_usuario_area;
+    
+    END LOOP; 
+        
+    RETURN; -- v_id_rol;
+END
+$function$
+;
+
+
+
+
+
+/*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
+
+
+CREATE OR REPLACE FUNCTION public.fn_sist_consulta_rol_usuario(integer)
+ RETURNS integer
+ LANGUAGE plpgsql
+AS $function$
+DECLARE 
+    v_id_rol INTEGER;
+BEGIN
+
+	/*CONSULA AREA*/
+    SELECT 
+      public.usuario_rol.nidrol INTO v_id_rol
+    FROM
+      public.usuario
+      INNER JOIN public.usuario_rol ON (public.usuario.nidusuario = public.usuario_rol.nidusuario)
+      WHERE public.usuario.nidusuario = $1 AND public.usuario_rol.bactivo= TRUE;
+    
+    RETURN v_id_rol;
+END
+$function$
+;
+
+
+
+
+/*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
+
+CREATE OR REPLACE FUNCTION public.fn_sist_listar_menu_usuario(p_usuario character varying)
+ RETURNS TABLE(nidmenu integer, bactivo boolean, dfechareg timestamp without time zone, nidsesion integer, siconomenu_m character varying, snombremenu_m character varying, surl_m character varying, nidmodulo integer, nidsubmenu integer, norden integer, siconomenu character varying, snombremenu character varying, surl character varying)
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+    return query 
+    
+    		 SELECT
+			  public.menu.nidmenu, 
+			  public.menu.bactivo, 
+			  public.menu.dfechareg, 
+			  public.menu.nidsesion, 
+			  public.menu.siconomenu AS siconomenu_m, 
+			  public.menu.snombremenu AS snombremenu_m, 
+			  public.menu.surl AS surl_m , 
+			  public.menu.nidmodulo ,
+			  public.submenu.nidsubmenu ,
+			  public.submenu.norden ,
+			  public.submenu.siconomenu ,
+			  public.submenu.snombremenu ,			  
+			  public.submenu.surl 
+			FROM
+			  public.usuario 
+			  INNER JOIN public.usuario_rol ON (public.usuario.nidusuario = public.usuario_rol.nidusuario) 
+			  INNER JOIN public.rol ON (public.usuario_rol.nidrol = public.rol.nidrol)
+			  INNER JOIN public.menu_rol ON (public.rol.nidrol = public.menu_rol.nidrol) 
+			  INNER JOIN public.menu ON (public.menu_rol.nidmenu = public.menu.nidmenu) 
+			  INNER JOIN public.submenu ON (public.submenu.nidmenu =public.menu.nidmenu)
+			WHERE public.usuario.slogin= p_usuario; 
+END
+$function$
+;
+
+
+
+
+
+
+/*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
+
+
+CREATE OR REPLACE FUNCTION public.salvar()
+ RETURNS boolean
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+COPY (SELECT * FROM public.sesion ) TO 'D:/archivo.csv' WITH CSV;
+RETURN true;
+END;
+$function$
+;
+
+
+
+
+
+/*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
